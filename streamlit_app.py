065f80f4043c507a17baff2c7c172e664bb4a9ee
HopeFan <ehesami72@gmail.com>
2023-01-21 23:23:41 +1100

    Update streamlit_app.py
    add breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
