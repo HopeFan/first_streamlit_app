8a734bbe16ebfdf5b3aaf14017cd3906b8e8ebe3
HopeFan <ehesami72@gmail.com>
2023-01-26 19:29:12 +1100

    add header and json

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
