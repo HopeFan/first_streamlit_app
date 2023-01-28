ee9300f04008a9d7f6727d4b2f7c551a2f141270
HopeFan <ehesami72@gmail.com>
2023-01-29 00:37:11 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,7 +42,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_row = my_cur.fetchall()
 streamlit.text("the fruit load container")
 streamlit.text(my_data_row)
 
