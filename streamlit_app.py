badd1fa4603ba36d83d2730c34816c26ab6a0d37
HopeFan <ehesami72@gmail.com>
2023-01-29 00:40:51 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ my_data_row = my_cur.fetchall()
 streamlit.text("the fruit load container")
 streamlit.text(my_data_row)
 
-fruit_choice = streamlit.text_input('What fruit would you like to add','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like to add','jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice)
