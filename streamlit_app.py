a370a02cb8a16bbe42bd6b7999525d718755044e
HopeFan <ehesami72@gmail.com>
2023-01-29 00:39:57 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
 
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -46,3 +47,5 @@ my_data_row = my_cur.fetchall()
 streamlit.text("the fruit load container")
 streamlit.text(my_data_row)
 
+fruit_choice = streamlit.text_input('What fruit would you like to add','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
